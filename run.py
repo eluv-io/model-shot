00b99cbabcbb29ad363f2448ec77a3cc01358d52
Nicholas Buoncristiani <nick.buoncristiani@eluv.io>
2024-12-06 00:53:14 +0000

    convert runtime config to dataclass

--- run.py
+++ run.py
@@ -2,10 +2,21 @@ import argparse
 from typing import List
 import os
 import json
-from dataclasses import asdict
+from dataclasses import asdict, dataclass
+from common_ml.types import Data
 
 from shot.model import ShotDetector
 from config import config
+
+
+# shot detection takes no config params
+@dataclass
+class RuntimeConfig(Data):
+    pass
+
+    @staticmethod
+    def from_dict(data: dict) -> 'RuntimeConfig':
+        return RuntimeConfig()
 
 def run(video_paths: List[str], runtime_config: str=None) -> None:
     shot_detector = ShotDetector(config["storage"]["transnet_path"], config["storage"]["test_card_path"])
@@ -19,7 +30,7 @@ def run(video_paths: List[str], runtime_config: str=None) -> None:
         out_fname = os.path.join(tags_out, f"{os.path.basename(fname).split('.')[0]}_tags.json")
         with open(out_fname, 'w') as f:
             f.write(json.dumps([asdict(tag) for tag in tags]))
-
+            
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('video_paths', nargs='+', type=str)
