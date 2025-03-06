f35fc61ed19acfb88aa964a5d5b8c76f3c712a78
elv-joeA <joe.anakata@eluv.io>
2025-03-06 22:50:56 +0000

    get shot weights from ml drive

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo keys:
+
 if ! ssh-add -l ; then
     echo ssh agent does not have any identities loaded, will not be able to build
     echo add them by running ssh-add on your local machine, or on the remote if you have keys there
@@ -7,4 +9,11 @@ if ! ssh-add -l ; then
     exit 1
 fi
 
-podman build --format docker -t shot . --network host --build-arg SSH_AUTH_SOCK=$SSH_AUTH_SOCK --volume "${SSH_AUTH_SOCK}:${SSH_AUTH_SOCK}"
+echo
+
+SCRIPT_PATH="$(dirname "$(realpath "$0")")"
+MODEL_PATH=$(yq -r .storage.model_path $SCRIPT_PATH/config.yml)
+
+rsync --progress --update --times --recursive --links --delete $MODEL_PATH/ $SCRIPT_PATH/models/
+
+podman build --format docker -t shot . --network host --build-arg SSH_AUTH_SOCK=/tmp/ssh-auth-sock --volume "${SSH_AUTH_SOCK}:/tmp/ssh-auth-sock"
