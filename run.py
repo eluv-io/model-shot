ad35513d9253eb4d24188e4567784676f7f99f5b
Nicholas Buoncristiani <nick.buoncristiani@eluv.io>
2026-03-25 22:53:01 +0000

    updated run.py to use catch_errors

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
-import argparse
-import os
+
 import json
 from dataclasses import dataclass
 import setproctitle
 from dacite import from_dict
 
-from common_ml.tagging.run_helpers import start_loop_from_av_model
+from common_ml.tagging.run_helpers import run_default, catch_errors, get_params
 
 from shot.model import ShotDetector
 from config import config
@@ -16,19 +15,13 @@ class RuntimeConfig:
     # otherwise we assume input files are contiguous segments of a larger video and we allow shots to span across input files. 
     contiguous: bool = True
 
-def _parse_config_string(config_str: str) -> RuntimeConfig:
-    config_dict = json.loads(config_str)
-    return from_dict(RuntimeConfig, config_dict)
-
 if __name__ == '__main__':
-    setproctitle.setproctitle('model-shot')    
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--output-path', type=str, required=True, help='Path to save the output tags')
-    parser.add_argument('--params', type=str, default=None, help='JSON string of parameters to override the default config')
-    args = parser.parse_args()
+    setproctitle.setproctitle('model-shot')
     
-    params = _parse_config_string(args.params) if args.params else RuntimeConfig()
+    catch_errors()
+    params = get_params()
+    params = from_dict(RuntimeConfig, params)
 
     model = ShotDetector(config["storage"]["transnet_path"], contiguous=params.contiguous)
 
-    start_loop_from_av_model(model, args.output_path)
+    run_default(model)
